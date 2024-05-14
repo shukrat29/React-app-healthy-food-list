a990113ed8da1e826a6850a1108c6555c5fdd757
shukra <shukratamang.npl@gmail.com>
2024-05-15 07:58:12 +1000

    css imported

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return <div>Healthy Food List</div>;
+  return (
+    <>
+      <h1>Healthy Food List</h1>
+      <ul class='list-group'>
+        <li class='list-group-item'>An item</li>
+        <li class='list-group-item'>A second item</li>
+        <li class='list-group-item'>A third item</li>
+        <li class='list-group-item'>A fourth item</li>
+        <li class='list-group-item'>And a fifth one</li>
+      </ul>
+    </>
+  );
 }
 
 export default App;
